0598c299134fe9d1d67d6dfe1e5b1b247f955c3a
Rezuan Hossain <rezuanhossain093@gmail.com>
2024-06-11 02:10:10 +0600

    added home & moviedetails page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const [movieData, setMovieData] = useState([]);
+
+  useEffect(() => {
+    const storedMovieData = JSON.parse(window.localStorage.getItem('movieData'));
+    if (storedMovieData) {
+      setMovieData(storedMovieData);
+    }
+  }, []);
+
+  return (
+    <>
+      <div className="row row-cols-3 g-3">
+        {movieData.length > 0 && movieData.map((movie, index) => (
+          <Link to={`/movies/${index}`} className="col" key={index}>
+            <div className="card">
+              <img src={movie.poster} className="card-img-top" alt="Poster" />
+              <div className="card-body">
+                <h5 className="card-title">{movie.movie_name}</h5>
+                <p className="card-text">{movie.description}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/pages/MovieDetail.jsx
@@ -0,0 +1,32 @@
+import  { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+const MovieDetail = () => {
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    const storedMovieData = JSON.parse(window.localStorage.getItem('movieData'))[movieId];
+
+    setMovie(storedMovieData);   
+  },[]);
+
+  return (
+    <div>
+      {movie && (
+        <div>
+            <h1>{movie.movie_name}</h1>
+            <img src={movie.poster} alt={movie.movie_name} />
+            <p>{movie.description}</p>
+            <p>Release Date: {new Date(movie.release_date).toLocaleDateString()}</p>
+            <p>Star Cast: {movie.star_cast}</p>
+            <p>Genre: {movie.genre}</p>
+            <p>Duration: {movie.duration}</p>
+            <p>Language: {movie.language}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MovieDetail;
